99476cc36e4a19c7f409936122242e1998d46aa7
Anonymous275 <36374260+Anonymous-275@users.noreply.github.com>
2022-03-08 14:52:51 +0200

    basics

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,16 @@
 /// Copyright (c) 2021-present Anonymous275 read the LICENSE file for more info.
 ///
 
-int main() {
+#include "duk_wrapper.h"
 
+int clog(duk_context* ctx) {
+    printf("print func -> %s\n", duk_safe_to_string(ctx, 0));
+    return 0;
+}
+
+int main() {
+    duktape engine;
+    engine.push_function("clog", clog, 1);
+    engine.push_file("../example.js");
     return 0;
 }
